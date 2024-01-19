d73f1994828c192ef241562009aa024a3cb54263
NoemiP94 <m_emi94@hotmail.it>
2024-01-19 10:14:25 +0100

    Initial commit

---
+++ src/main/java/noemipusceddu/U2W2L5be/U2W2L5beApplication.java
@@ -0,0 +1,13 @@
+package noemipusceddu.U2W2L5be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2L5beApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2L5beApplication.class, args);
+	}
+
+}
